2365425755d0c0600835e05e593f749888fa922d
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-11-16 14:17:48 -0600

    Update MainActivity for implement RecyclerView ViewHolder and RecyclerView Adapter.

--- app/src/main/java/com/example/knowyourgovernment/MainActivity.java
+++ app/src/main/java/com/example/knowyourgovernment/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.knowyourgovernment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
+
+    private final List<Official> officialsList = new ArrayList<>();  // Main content is here
+
+    private RecyclerView recyclerView; // Layout's recyclerview
+
+    private RecyclerViewAdapter mAdapter; // Data to recyclerview adapter
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler);
+        mAdapter = new RecyclerViewAdapter(officialsList, this);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Make some data - not always needed - just used to fill list
+        for (int i = 0; i < 20; i++) {
+            officialsList.add(new Official());
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
     }
 }
