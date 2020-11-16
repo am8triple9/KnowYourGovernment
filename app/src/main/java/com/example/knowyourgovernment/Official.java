e9a252fb2902ea96b1ad144762809e3e08af417f
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-11-16 11:16:50 -0600

    Create Official class

---
+++ app/src/main/java/com/example/knowyourgovernment/Official.java
@@ -0,0 +1,124 @@
+package com.example.knowyourgovernment;
+
+
+public class Official {
+
+    private String city;
+    private String state;
+    private String zip;
+    private String officeName;
+    private String officialIndex;
+    private String name;
+    private String address;
+    private String party;
+    private String phones;
+    private String urls;
+    private String emails;
+    private String photoUrl;
+    private String[] channels;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+    public String getPhones() {
+        return phones;
+    }
+
+    public void setPhones(String phones) {
+        this.phones = phones;
+    }
+
+    public String getUrls() {
+        return urls;
+    }
+
+    public void setUrls(String urls) {
+        this.urls = urls;
+    }
+
+    public String getEmails() {
+        return emails;
+    }
+
+    public void setEmails(String emails) {
+        this.emails = emails;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String[] getChannels() {
+        return channels;
+    }
+
+    public void setChannels(String[] channels) {
+        this.channels = channels;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getOfficeName() {
+        return officeName;
+    }
+
+    public void setOfficeName(String officeName) {
+        this.officeName = officeName;
+    }
+
+    public String getOfficialIndex() {
+        return officialIndex;
+    }
+
+    public void setOfficialIndex(String officialIndex) {
+        this.officialIndex = officialIndex;
+    }
+}
