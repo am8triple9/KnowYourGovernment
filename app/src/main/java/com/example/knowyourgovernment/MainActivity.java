87c020ae1157bbd7764918765d4d3186bb8abfc0
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-11-17 11:17:18 -0600

    Create default location data.

--- app/src/main/java/com/example/knowyourgovernment/MainActivity.java
+++ app/src/main/java/com/example/knowyourgovernment/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.knowyourgovernment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
     private final List<Official> officialsList = new ArrayList<>();  // Main content is here
-
     private RecyclerView recyclerView; // Layout's recyclerview
-
     private RecyclerViewAdapter mAdapter; // Data to recyclerview adapter
+    private static int MY_LOCATION_REQUEST_CODE_ID = 111;
+    private LocationManager locationManager;
+    private Criteria criteria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        getLocation();
+
         //Make some data - not always needed - just used to fill list
         for (int i = 0; i < 20; i++) {
             Official dummy = new Official();
@@ -40,6 +57,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             dummy.setName("Name " + (i + 1));
             officialsList.add(dummy);
         }
+    }
+
+    private void getLocation() {
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        criteria = new Criteria();
+
+        // use gps for location
+        // criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        // criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        // use network for location
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    MY_LOCATION_REQUEST_CODE_ID);
+        } else {
+            setLocation();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull
+            String[] permissions, @NonNull
+                    int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_LOCATION_REQUEST_CODE_ID) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PERMISSION_GRANTED) {
+                setLocation();
+                return;
+            }
+        }
+        ((TextView) findViewById(R.id.locationData)).setText("No Permission");
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setLocation() {
+
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        //((TextView) findViewById(R.id.bestText)).setText(bestProvider);
+
+        Location currentLocation = null;
+        if (bestProvider != null) {
+            currentLocation = locationManager.getLastKnownLocation(bestProvider);
+        }
+        if (currentLocation != null) {
+            ((TextView) findViewById(R.id.locationData)).setText(
+                    String.format(Locale.getDefault(),
+                            "%.4f, %.4f", currentLocation.getLatitude(), currentLocation.getLongitude()));
+        } else {
+            ((TextView) findViewById(R.id.locationData)).setText("No Data For Location");
+        }
+
+
     }
 
     @Override
